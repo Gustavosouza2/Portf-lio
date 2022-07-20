3ffa6765c0ec4b011181d748cb0dbb0aa618f748
Gustavo Souza <gustavoleonsouza@gmail.com>
2022-07-20 11:34:05 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const colors = ["#FF8008", "#93F9B9", "#EB3349", "#DD5E89", "#EDDE5D", "#7AA1D2", "#DD2476", "#EDDE5D"];
+
+const numBalls = 97;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 80)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+  
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 5 === 0 ? -11 : 11),
+    y: Math.random() * 12
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out"
+    }
+  );
+  if (window.matchMedia("(max-width: 767px)").matches) {
+  }
+  else {
+  }
+});
---
+++ scroll.js
@@ -0,0 +1,12 @@
+window.sr = ScrollReveal({ reset:true});
+ScrollReveal({ distance: '15px' });
+sr.reveal('.sidebar', {duration : 3000, origin: 'left'})
+sr.reveal('.home', {duration : 3000, origin: 'right'})
+sr.reveal('.social-midia', {duration : 3000, origin: 'left'})
+sr.reveal('.desenho', {duration : 3000, origin: 'right'})
+sr.reveal('.sessao-eu', {duration : 3000,  origin: 'left'})
+sr.reveal('.sessao-habilidades h1', {duration : 3000,  origin: 'left'})
+sr.reveal('.icones', {duration : 3000,  origin: 'right'})
+sr.reveal('.sessao-projetos h1', {duration : 3000, origin:'left'})
+sr.reveal('.card', {duration : 3000, origin:'bottom'})
+sr.reveal('.footer-content', {duration : 3000, origin:'right'})
